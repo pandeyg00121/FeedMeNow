0bf1155b7a35bfd1ff498d609e84653baf6321b4
pandeyg00121 <pranaypandey00121@gmail.com>
2024-03-09 23:14:33 +0530

    Food Model designed
    Food Schema designed with menu options.

---
+++ server/controllers/foodController.js
@@ -0,0 +1,25 @@
+const Food = require("./../models/userModel");
+const catchAsync = require("./../utils/catchAsync");
+const AppError = require("./../utils/appError");
+
+exports.getAllFoods = catchAsync(async(req,res,next)=>{
+    const food = await Food.find();
+
+    res.status(200).json({
+        status: "success",
+        data: {
+          food,
+        },
+    });
+});
+
+exports.createFood = catchAsync(async(req,res,next)=>{
+    const newFood = await Food.create(req.body);
+  
+    res.status(201).json({
+      status: 'success',
+      data: {
+        newFood
+      }
+    });
+});
