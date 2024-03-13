636a5c23210c910a8684ff00f46ac77547f7c881
tripathi08 <priyanshu.tri67@gmail.com>
2024-03-14 02:49:46 +0530

    Used RTK Query
    Used RTK query along with cors to connect client with server.

--- client/src/redux/api.js
+++ client/src/redux/api.js
@@ -4,9 +4,29 @@ export const myApi=createApi({
     reducerPath:"api",
     baseQuery:fetchBaseQuery({baseUrl:process.env.REACT_APP_BASE_URL}),
     endpoints:(builder)=>({
-      getFoods:builder.query({query:()=>"foods"})
-    })
+      //Get requests
+      getHomeRestaurants:builder.query({query:()=>"top-5-restaurants"}),
+      getHomeFoods:builder.query({query:()=>"top-6-foods"}),
+      getAllMenuItems:builder.query({query:()=>"/restaurants/manageItems"}),
+      getPreviousOrders:builder.query({query:()=>"/restaurants/manageOrders/previous"}),
+      getCurrentOrders:builder.query({query:()=>"/restaurants/manageOrders/current"}),
+      getAllUsers:builder.query({query:()=>"/admin/allUsers"}),
+      getAllRestaurants:builder.query({query:()=>"/admin/allRes"}),
+      getPendingReqAdmin:builder.query({query:()=>"/admin/pendingReq"}),
+      getAllReviews:builder.query({query:()=>"/admin/allReviews"}),
+      getMyCart:builder.query({query:()=>"/users/cart"}),
+      //Post requests
+      newFoodItem:builder.mutation({
+        query:(food)=>({
+          url:"/restaurants/addItem",
+          method:"POST",
+          body:food,
+        })
+      }),
+      
+      
+     })
 })
 
-export const {useGetFoodsQuery}=myApi;
+export const {useGetHomeFoodsQuery,useGetHomeRestaurantsQuery,useNewFoodItemMutation}=myApi;
 
